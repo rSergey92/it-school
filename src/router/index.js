2cfe62cdaef0c68a13d02905c4e1c4b30af42a40
Sergey Romanov <sergejj.romanov@vseinstrumenti.ru>
2021-11-11 00:10:23 +0300

    Добавлены новые компоненты, хдебные крошки, модалка, общая стилизация

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Task from '../views/Task.vue'
+import Home from '@/views/Home.vue'
+import TaskList from '@/views/TaskList';
+import Task from '@/views/Task.vue'
+import Analytics from '@/views/Analytics.vue'
 import notFound from '@/views/notFound';
 
 Vue.use(VueRouter)
@@ -14,16 +16,44 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      breadcrumb: [
+        { name: 'Главная' },
+      ],
+    },
   },
   {
     path: '/tasks',
     name: 'TasksList',
-    component: () => import(/* webpackChunkName: "about" */ '../views/TaskList.vue')
+    component: TaskList,
+    meta: {
+      breadcrumb: [
+        { name: 'Главная', link: '/' },
+        { name: 'Список заявок' },
+      ],
+    },
   },
   {
     path: '/task/:id',
-    component: Task
+    component: Task,
+    meta: {
+      breadcrumb: [
+        { name: 'Главная', link: '/' },
+        { name: 'Список заявок', link: '/tasks' },
+        { name: 'Заявка' },
+      ],
+    },
+  },
+  {
+    path: '/analytics',
+    component: Analytics,
+    meta: {
+      breadcrumb: [
+        { name: 'Главная', link: '/' },
+        { name: 'Аналитика' },
+      ],
+    },
   }
 ]
 
